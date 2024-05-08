b08010b148fb45721349cd1678d04abf8d5d134a
Gabriela Ruiz <gabrielazenm@gmail.com>
2024-05-08 05:41:26 -0500

    Cambios nuevos ingresos mayo seeders

--- database/seeders/tipo_ingresoSeeder.php
+++ database/seeders/tipo_ingresoSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Tipo_ingreso;
+use App\Models\Tipo_Ingreso;
 
 class tipo_ingresoSeeder extends Seeder
 {
